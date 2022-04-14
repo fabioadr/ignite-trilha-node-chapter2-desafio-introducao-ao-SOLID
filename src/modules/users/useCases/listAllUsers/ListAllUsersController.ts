b1f7e19c38c51834a7ca4aa1685fa03670e3864a
Fábio Agapito Diniz Ribas <fabiodiniz@MacBook-Air-de-Fabio.local>
2022-04-14 09:20:21 -0300

    Tarefas Concluidas

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,21 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase'
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.header('user_id')
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id })
+
+      return response.status(200).json(users)
+    } catch (error) {
+      return response.status(400).json({ error: error.message })
+    }
   }
 }
 
-export { ListAllUsersController };
+export { ListAllUsersController }
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,13 +1,21 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
-import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+import { TurnUserAdminUseCase } from './TurnUserAdminUseCase'
 
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params
+
+    try {
+      const users = this.turnUserAdminUseCase.execute({ user_id })
+
+      return response.status(200).json(users)
+    } catch (error) {
+      return response.status(404).json({ error: error.message })
+    }
   }
 }
 
-export { TurnUserAdminController };
+export { TurnUserAdminController }
